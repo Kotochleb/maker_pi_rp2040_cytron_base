dd7596469fc3081e2e834cf892adf00bc434bbc0
Krzysztof Wojciechowski <krzy.wojciecho@gmail.com>
2022-02-27 14:28:59 +0100

    Finish template setup

--- include/maker_pi_pins.h
+++ include/maker_pi_pins.h
@@ -2,45 +2,44 @@
 #define __MAKER_PI_PINOUT__
 
 namespace maker_pi {
-    namespace pins {
-        // servo motors
-        static const unsigned int SERVO_0 = 12;
-        static const unsigned int SERVO_1 = 13;
-        static const unsigned int SERVO_2 = 14;
-        static const unsigned int SERVO_3 = 15;
+    // servo motors
+    static const unsigned int SERVO_0 = 12;
+    static const unsigned int SERVO_1 = 13;
+    static const unsigned int SERVO_2 = 14;
+    static const unsigned int SERVO_3 = 15;
 
-        // dc motors
-        static const unsigned int MOTOR_1_A = 8;
-        static const unsigned int MOTOR_1_B = 9;
-        static const unsigned int MOTOR_2_A = 10;
-        static const unsigned int MOTOR_2_B = 11;
+    // dc motors
+    static const unsigned int MOTOR_1_A = 8;
+    static const unsigned int MOTOR_1_B = 9;
+    static const unsigned int MOTOR_2_A = 10;
+    static const unsigned int MOTOR_2_B = 11;
 
-        // buttons
-        static const unsigned int BUTTON_GP_20 = 20;
-        static const unsigned int BUTTON_GP_21 = 21;
+    // buttons
+    static const unsigned int BUTTON_GP_20 = 20;
+    static const unsigned int BUTTON_GP_21 = 21;
 
-        // misc
-        static const unsigned int BUZZER = 11;
-        static const unsigned int RGB_DIODE = 22;
-        static const unsigned int BATTER_ADC = 29;
+    // misc
+    static const unsigned int BUZZER = 11;
+    static const unsigned int RGB_DIODE = 22;
+    static const unsigned int BATTER_ADC = 29;
 
-        // groove
-        static const unsigned int GROOVE_1_A = 0;
-        static const unsigned int GROOVE_1_B = 1;
-        static const unsigned int GROOVE_2_A = 2;
-        static const unsigned int GROOVE_2_B = 3;
-        static const unsigned int GROOVE_3_A = 4;
-        static const unsigned int GROOVE_3_B = 5;
-        static const unsigned int GROOVE_4_A = 16;
-        static const unsigned int GROOVE_4_B = 17;
-        static const unsigned int GROOVE_5_A = 6;
-        static const unsigned int GROOVE_5_B = 26;
-        static const unsigned int GROOVE_6_A = 26;
-        static const unsigned int GROOVE_6_B = 27;
-        static const unsigned int GROOVE_7_A = 7;
-        static const unsigned int GROOVE_7_B = 28;
-
-    } // namespace pins    
+    // groove
+    static const unsigned int GROOVE_1_A = 0;
+    static const unsigned int GROOVE_1_B = 1;
+    static const unsigned int GROOVE_2_A = 2;
+    static const unsigned int GROOVE_2_B = 3;
+    static const unsigned int GROOVE_3_A = 4;
+    static const unsigned int GROOVE_3_B = 5;
+    static const unsigned int GROOVE_4_A = 16;
+    static const unsigned int GROOVE_4_B = 17;
+    static const unsigned int GROOVE_5_A = 6;
+    static const unsigned int GROOVE_5_B = 26;
+    static const unsigned int GROOVE_6_A = 26;
+    static const unsigned int GROOVE_6_B = 27;
+    static const unsigned int GROOVE_7_A = 7;
+    static const unsigned int GROOVE_7_B = 28;
+  
+    // constant for PWM
     static const int MAX_PWM = 65535;
 } // namespace maker_pi    
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,24 @@
 #include <Arduino.h>
-#include <PID_v1.h>
-
-#include "pio_rotary_encoder.h"
-
-// initialise static encoder variables for both motors
-int RotaryEncoder::rotation_motor_a = 0;
-int RotaryEncoder::rotation_motor_b = 0;
-
-// create motor object
-RotaryEncoder my_encoder_a(2, 3,   MOTOR_A_SM);
-RotaryEncoder my_encoder_b(26, 27, MOTOR_B_SM);
-
-// variables to measure equal time between measurements
-long previousMillis = 0;
-long currentMillis = 0;
-// print data every 0.1 s
-int interval = 100;
-
-// values to keep computing motor velocity
-int last_rotation_a;
-int last_rotation_b;
-
-
-double Setpoint_a, Input_a, Output_a;
-double Setpoint_b, Input_b, Output_b;
-PID myPID_a(&Input_a, &Output_a, &Setpoint_a, 0.001, 0.001, 0.0, DIRECT);
-PID myPID_b(&Input_b, &Output_b, &Setpoint_b, 0.001, 0.001, 0.0, DIRECT);
+#include "maker_pi_pins.h"
 
 
 void setup() {
   // initialise serial port
   Serial.begin(115200); 
-  
-  // set initial reading of encoders to 0
-  my_encoder_a.set_rotation(0);
-  my_encoder_b.set_rotation(0); 
-
-  pinMode(10, OUTPUT);
-  pinMode(8, OUTPUT);
-  myPID_a.SetMode(AUTOMATIC);
-  myPID_b.SetMode(AUTOMATIC);
-  Setpoint_a = 69.0;
-  Setpoint_b = 420.0;
+  // initialise LED pin
+  pinMode(maker_pi::GROOVE_1_A, OUTPUT);
 }
- 
+
+
 void loop() {
-  // get current time
-  currentMillis = millis();
-
-  // check is measured interval has passed
-  if (currentMillis - previousMillis > interval) {
- 
-    previousMillis = currentMillis;
-
-    // get encoder readings
-    int current_rotation_a = my_encoder_a.get_rotation();
-    int current_rotation_b = my_encoder_b.get_rotation();
-    
-    // print measurements
-    printf("Motor A: %d\t", last_rotation_a - current_rotation_a);
-    printf("Output:  %f\t", Output_a);
-    printf("Motor B: %d\t", last_rotation_b- current_rotation_b);
-    printf("Output:  %f",   Output_b);
-    printf("\n");
-
-    Input_a = double(last_rotation_a - current_rotation_a);
-    myPID_a.Compute();
-    analogWrite(10, int(Output_a*65535));
-
-    Input_b = double(last_rotation_b - current_rotation_b);
-    myPID_b.Compute();
-    analogWrite(8, int(Output_b*65535));
-
-    // update last values
-    last_rotation_a = current_rotation_a;
-    last_rotation_b = current_rotation_b;   
-
-  }
+  // turn on LED on pin assigned to GROOVE 1 pin num 0
+  digitalWrite(maker_pi::GROOVE_1_A, HIGH);
+  // print pin state
+  printf("Pin is set to low\n");
+  // wait half of a second
+  delay(500);
+  // turn of the LED
+  digitalWrite(maker_pi::GROOVE_1_A, LOW);
+  printf("Pin is set to high\n");
+  delay(500);
 }
